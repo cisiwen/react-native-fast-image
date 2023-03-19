a051d59af61660f2b1b152922b3e55c97e11ab99
Xiaozhi Weng <cisiwen@hotmail.com>
2023-03-19 23:56:11 +1100

    add media store loader

--- android/src/main/java/com/dylanvann/fastimage/FastImageGlideModule.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageGlideModule.java
@@ -1,14 +1,18 @@
 package com.dylanvann.fastimage;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 
+import com.bumptech.glide.Glide;
 import com.bumptech.glide.GlideBuilder;
+import com.bumptech.glide.Registry;
 import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.load.engine.bitmap_recycle.LruBitmapPool;
 import com.bumptech.glide.load.engine.cache.InternalCacheDiskCacheFactory;
 import com.bumptech.glide.load.engine.cache.LruResourceCache;
 import com.bumptech.glide.load.engine.cache.MemorySizeCalculator;
 import com.bumptech.glide.module.AppGlideModule;
+import com.dylanvann.fastimage.mediastore.MediaStoreLoaderFactory;
 
 // We need an AppGlideModule to be present for progress events to work.
 @GlideModule
@@ -25,5 +29,10 @@ public final class FastImageGlideModule extends AppGlideModule {
         builder.setMemoryCache(new LruResourceCache(customMemoryCacheSize));
         builder.setBitmapPool(new LruBitmapPool(customBitmapPoolSize));
         builder.setDiskCache(new InternalCacheDiskCacheFactory(context, 1000*1024*1024));
+    }
+
+    @Override
+    public void registerComponents(Context context, Glide glide, Registry registry) {
+        registry.prepend(String.class, Bitmap.class, new MediaStoreLoaderFactory(context));
     }
 }
